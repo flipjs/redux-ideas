9292cf02c9da30fadde9de4c9e545904e4486184
Felipe Apostol <flipjs.io@gmail.com>
2016-04-24 16:11:36 +0100

    add sample index in api directory

---
+++ app/api/index.js
@@ -0,0 +1,14 @@
+import fetchUsers from 'fetchUsers'
+import fetchUser from 'fetchUser'
+import updateUser from 'updateUser'
+import deleteUser from 'deleteUser'
+
+const api = {
+  fetchUsers,
+  fetchUser,
+  updateUser,
+  deleteUser
+}
+
+export default api
+
